917a613c0b716b52f9b1f58b62fc5a984e614710
Kevin202510 <72065473+Kevin202510@users.noreply.github.com>
2023-03-02 21:28:14 +0800

    home update

--- pages/script.php
+++ pages/script.php
@@ -32,6 +32,7 @@
 
     <!-- Custom scripts for all pages-->
     <script src="../assets/js/sb-admin-2.min.js"></script>
+    <script src="../assets/vendor/chart.js/Chart.min.js"></script>
 
     <script>
         $(document).ready(function(){
