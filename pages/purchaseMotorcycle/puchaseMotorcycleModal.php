61e15f3e9eb2ddf25344db0f4b40e00c44f29424
Kevin202510 <72065473+Kevin202510@users.noreply.github.com>
2023-03-05 19:53:05 +0800

    update

--- pages/purchaseMotorcycle/puchaseMotorcycleModal.php
+++ pages/purchaseMotorcycle/puchaseMotorcycleModal.php
@@ -16,6 +16,7 @@
 	<!-- CSS Files -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
 	<link href="assets/css/gsdk-bootstrap-wizard.css" rel="stylesheet" />
+
 	<!-- CSS Just for demo purpose, don't include it in your project -->
 	<link href="assets/css/demo.css" rel="stylesheet" />
 </head>
@@ -64,16 +65,6 @@
                             <div class="tab-pane" id="about">
                               <div class="row">
                                   <h4 class="info-text"> Let's start with the basic information (with validation)</h4>
-                                  <div class="col-sm-4 col-sm-offset-1">
-                                     <div class="picture-container">
-                                          <div class="picture">
-                                              <img src="assets/img/default-avatar.png" class="picture-src" id="wizardPicturePreview" title=""/>
-                                              <input type="file" id="wizard-picture">
-                                          </div>
-                                          <h6>Choose Picture</h6>
-                                      </div>
-                                  </div>
-                                  <div class="row">
                                   <div class="col-sm-6">
                                       <div class="form-group">
                                         <label>First Name <small>(required)</small></label>
@@ -84,11 +75,34 @@
                                         <input name="lastname" type="text" class="form-control" placeholder="Smith...">
                                       </div>
                                   </div>
-                                  </div>
-                                  <div class="col-sm-10 col-sm-offset-1">
+                                  <div class="col-sm-6">
                                       <div class="form-group">
-                                          <label>Email <small>(required)</small></label>
-                                          <input name="email" type="email" class="form-control" placeholder="andrew@creative-tim.com">
+                                        <label>First Name <small>(required)</small></label>
+                                        <input name="firstname" type="text" class="form-control" placeholder="Andrew...">
+                                      </div>
+                                      <div class="form-group">
+                                        <label>Last Name <small>(required)</small></label>
+                                        <input name="lastname" type="text" class="form-control" placeholder="Smith...">
+                                      </div>
+                                  </div>
+                                  <div class="col-sm-6">
+                                      <div class="form-group">
+                                        <label>First Name <small>(required)</small></label>
+                                        <input name="firstname" type="text" class="form-control" placeholder="Andrew...">
+                                      </div>
+                                      <div class="form-group">
+                                        <label>Last Name <small>(required)</small></label>
+                                        <input name="lastname" type="text" class="form-control" placeholder="Smith...">
+                                      </div>
+                                  </div>
+                                  <div class="col-sm-6">
+                                      <div class="form-group">
+                                        <label>First Name <small>(required)</small></label>
+                                        <input name="firstname" type="text" class="form-control" placeholder="Andrew...">
+                                      </div>
+                                      <div class="form-group">
+                                        <label>Last Name <small>(required)</small></label>
+                                        <input name="lastname" type="text" class="form-control" placeholder="Smith...">
                                       </div>
                                   </div>
                               </div>
